ad7c6e83578c14127a81bb856b3b49fe200102c2
awerenion <zubkov.roman00@mail.ru>
2021-10-28 16:36:51 +0400

    add blanc

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div id="app">
+    <TodoBlank />
   </div>
 </template>
 
 <script>
+import TodoBlank from '@/components/TodoBlank.vue'
 
 export default {
   name: 'App',
   components: {
+    TodoBlank
   }
 }
 </script>
 
+    TodoBlanc
 <style>
-
+  body {
+    background: #24c6dc;
+    background: -webkit-linear-gradient(to right, #24c6dc, #514a9d);
+    background: linear-gradient(to right, #24c6dc, #514a9d);
+  }
 </style>
---
+++ src/components/TodoBlank.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="blanc">
+    <h1 class="title">Todo App</h1>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+  .blanc {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    width: 450px;
+    height: 550px;
+    background: white;
+    margin: -275px 0 0 -225px;
+    border-radius: 10px;
+  }
+
+  .title {
+    margin: 20px 0px 5px 25px;
+  }
+</style>
